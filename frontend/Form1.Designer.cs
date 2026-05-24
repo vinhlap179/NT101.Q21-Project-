3e4f5cc9abd3ca98ebfa9ec95bf74e18b09f8ac2
Dang Van Dang <makid1523@gmail.com>
2026-05-24 17:45:30 +0700

    remove unused code

--- frontend/Form1.Designer.cs
+++ frontend/Form1.Designer.cs
@@ -28,25 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             gpnlSidebar = new Guna.UI2.WinForms.Guna2Panel();
             guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
-            btnAbout = new Guna.UI2.WinForms.Guna2Button();
             btnRSA = new Guna.UI2.WinForms.Guna2Button();
             btnPlayFair = new Guna.UI2.WinForms.Guna2Button();
             lblSubLogo = new Guna.UI2.WinForms.Guna2HtmlLabel();
@@ -64,17 +61,16 @@
             // gpnlSidebar
             // 
             gpnlSidebar.Controls.Add(guna2Button1);
-            gpnlSidebar.Controls.Add(btnAbout);
             gpnlSidebar.Controls.Add(btnRSA);
             gpnlSidebar.Controls.Add(btnPlayFair);
             gpnlSidebar.Controls.Add(lblSubLogo);
             gpnlSidebar.Controls.Add(lblLogo);
-            gpnlSidebar.CustomizableEdges = customizableEdges9;
+            gpnlSidebar.CustomizableEdges = customizableEdges7;
             gpnlSidebar.Dock = DockStyle.Left;
             gpnlSidebar.FillColor = Color.FromArgb(17, 24, 39);
             gpnlSidebar.Location = new Point(0, 0);
             gpnlSidebar.Name = "gpnlSidebar";
-            gpnlSidebar.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            gpnlSidebar.ShadowDecoration.CustomizableEdges = customizableEdges8;
             gpnlSidebar.Size = new Size(223, 786);
             gpnlSidebar.TabIndex = 0;
             // 
@@ -96,28 +92,10 @@
             guna2Button1.TabIndex = 6;
             guna2Button1.Text = "NT101.Q21 Project";
             // 
-            // btnAbout
-            // 
-            btnAbout.BorderRadius = 12;
-            btnAbout.CustomizableEdges = customizableEdges3;
-            btnAbout.DisabledState.BorderColor = Color.DarkGray;
-            btnAbout.DisabledState.CustomBorderColor = Color.DarkGray;
-            btnAbout.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            btnAbout.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            btnAbout.FillColor = Color.FromArgb(31, 41, 55);
-            btnAbout.Font = new Font("Segoe UI", 9.857143F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnAbout.ForeColor = Color.White;
-            btnAbout.Location = new Point(20, 347);
-            btnAbout.Name = "btnAbout";
-            btnAbout.ShadowDecoration.CustomizableEdges = customizableEdges4;
-            btnAbout.Size = new Size(180, 45);
-            btnAbout.TabIndex = 5;
-            btnAbout.Text = "About";
-            // 
             // btnRSA
             // 
             btnRSA.BorderRadius = 12;
-            btnRSA.CustomizableEdges = customizableEdges5;
+            btnRSA.CustomizableEdges = customizableEdges3;
             btnRSA.DisabledState.BorderColor = Color.DarkGray;
             btnRSA.DisabledState.CustomBorderColor = Color.DarkGray;
             btnRSA.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -127,7 +105,7 @@
             btnRSA.ForeColor = Color.White;
             btnRSA.Location = new Point(20, 277);
             btnRSA.Name = "btnRSA";
-            btnRSA.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            btnRSA.ShadowDecoration.CustomizableEdges = customizableEdges4;
             btnRSA.Size = new Size(180, 45);
             btnRSA.TabIndex = 4;
             btnRSA.Text = " RSA Cipher";
@@ -135,7 +113,7 @@
             // btnPlayFair
             // 
             btnPlayFair.BorderRadius = 12;
-            btnPlayFair.CustomizableEdges = customizableEdges7;
+            btnPlayFair.CustomizableEdges = customizableEdges5;
             btnPlayFair.DisabledState.BorderColor = Color.DarkGray;
             btnPlayFair.DisabledState.CustomBorderColor = Color.DarkGray;
             btnPlayFair.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -145,7 +123,7 @@
             btnPlayFair.ForeColor = Color.White;
             btnPlayFair.Location = new Point(20, 210);
             btnPlayFair.Name = "btnPlayFair";
-            btnPlayFair.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            btnPlayFair.ShadowDecoration.CustomizableEdges = customizableEdges6;
             btnPlayFair.Size = new Size(180, 45);
             btnPlayFair.TabIndex = 3;
             btnPlayFair.Text = "PlayFair Cipher";
@@ -180,7 +158,7 @@
             lblPageDesc.ForeColor = SystemColors.ButtonFace;
             lblPageDesc.Location = new Point(777, 52);
             lblPageDesc.Name = "lblPageDesc";
-            lblPageDesc.Size = new Size(62, 32);
+            lblPageDesc.Size = new Size(45, 22);
             lblPageDesc.TabIndex = 7;
             lblPageDesc.Text = " Mô Tả";
             lblPageDesc.Click += lblPageDesc_Click;
@@ -189,24 +167,24 @@
             // 
             pnlMain.Controls.Add(pnlContent);
             pnlMain.Controls.Add(pnlHeader);
-            pnlMain.CustomizableEdges = customizableEdges15;
+            pnlMain.CustomizableEdges = customizableEdges13;
             pnlMain.Dock = DockStyle.Fill;
             pnlMain.FillColor = Color.FromArgb(243, 246, 251);
             pnlMain.Location = new Point(223, 0);
             pnlMain.Name = "pnlMain";
             pnlMain.Padding = new Padding(25);
-            pnlMain.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            pnlMain.ShadowDecoration.CustomizableEdges = customizableEdges14;
             pnlMain.Size = new Size(1029, 786);
             pnlMain.TabIndex = 1;
             // 
             // pnlContent
             // 
-            pnlContent.CustomizableEdges = customizableEdges11;
+            pnlContent.CustomizableEdges = customizableEdges9;
             pnlContent.Dock = DockStyle.Fill;
             pnlContent.FillColor = Color.FromArgb(243, 246, 251);
             pnlContent.Location = new Point(25, 93);
             pnlContent.Name = "pnlContent";
-            pnlContent.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            pnlContent.ShadowDecoration.CustomizableEdges = customizableEdges10;
             pnlContent.Size = new Size(979, 668);
             pnlContent.TabIndex = 1;
             // 
@@ -214,12 +192,12 @@
             // 
             pnlHeader.Controls.Add(lblPageDesc);
             pnlHeader.Controls.Add(lblPageTitle);
-            pnlHeader.CustomizableEdges = customizableEdges13;
+            pnlHeader.CustomizableEdges = customizableEdges11;
             pnlHeader.Dock = DockStyle.Top;
             pnlHeader.FillColor = Color.FromArgb(243, 246, 251);
             pnlHeader.Location = new Point(25, 25);
             pnlHeader.Name = "pnlHeader";
-            pnlHeader.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            pnlHeader.ShadowDecoration.CustomizableEdges = customizableEdges12;
             pnlHeader.Size = new Size(979, 68);
             pnlHeader.TabIndex = 0;
             // 
@@ -230,13 +208,12 @@
             lblPageTitle.ForeColor = Color.FromArgb(17, 24, 39);
             lblPageTitle.Location = new Point(339, 3);
             lblPageTitle.Name = "lblPageTitle";
-            lblPageTitle.Size = new Size(266, 52);
+            lblPageTitle.Size = new Size(185, 38);
             lblPageTitle.TabIndex = 0;
             lblPageTitle.Text = "PlayFair Cipher\n";
             // 
             // FrmMain
             // 
-            AutoScaleDimensions = new SizeF(13F, 31F);
             AutoScaleMode = AutoScaleMode.None;
             BackColor = Color.FromArgb(243, 246, 251);
             ClientSize = new Size(1252, 786);
@@ -259,7 +236,6 @@
 
         private Guna.UI2.WinForms.Guna2Panel gpnlSidebar;
         private Guna.UI2.WinForms.Guna2HtmlLabel lblLogo;
-        private Guna.UI2.WinForms.Guna2Button btnAbout;
         private Guna.UI2.WinForms.Guna2Button btnRSA;
         private Guna.UI2.WinForms.Guna2Button btnPlayFair;
         private Guna.UI2.WinForms.Guna2HtmlLabel lblSubLogo;
